d2e7d33663b5e1228ab256e6d671143fe761d92b
abhiguptaporwal-collab <abhiguptaporwal@gmail.com>
2026-02-14 22:47:28 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("WhatsApp Webhook Running 🚀");
+});
+
+app.post("/webhook", async (req, res) => {
+  console.log(req.body);
+
+  res.status(200).send("Received");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server started on port " + PORT);
+});

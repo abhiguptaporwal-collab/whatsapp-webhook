506b55f5473446068d3ad5c6f9c11dea0f519f1e
abhiguptaporwal-collab <abhiguptaporwal@gmail.com>
2026-02-15 09:56:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const express = require("express");
-
 const app = express();
+
 app.use(express.json());
 
+// Healthcheck route
 app.get("/", (req, res) => {
-  res.send("WhatsApp Webhook Running 🚀");
+  res.status(200).send("Server running");
 });
 
+// Webhook route
 app.post("/webhook", (req, res) => {
   console.log(req.body);
   res.status(200).send("OK");
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server started on port " + PORT);
+  console.log(`Server started on port ${PORT}`);
 });

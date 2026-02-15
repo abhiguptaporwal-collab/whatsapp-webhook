1b362598a64e063cbdb5d150f070a2f0ddbf48c9
abhiguptaporwal-collab <abhiguptaporwal@gmail.com>
2026-02-15 14:55:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 10000;
 
-const API_KEY = "sk_17d14a99dcf64c40a8665e2ecbd71f01";
+const API_KEY = "zjsyuafmiumjxibdxplvtiwljkomijss";
 const SOURCE_NUMBER = "919243166429"; // display_phone_number
 
 app.get("/", (req, res) => {
